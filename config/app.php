21ac5f8be4d54fa71ebde3df3c33a001e7b7c246
Sutrisno42 <siinoy42@gmail.com>
2026-01-29 10:50:51 +0700

    test

--- config/app.php
+++ config/app.php
@@ -48,7 +48,7 @@ return [
     |
     | This URL is used by the console to properly generate URLs when using
     | the Artisan command line tool. You should set this to the root of
-    | the application so that it's available within Artisan commands.
+    | the application so that it's available within Artisan commandss.
     |
     */
 
